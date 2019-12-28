e6bbf7409b3b8710195e0e3fd7212c97241b897d
Rax <damnloardbruh@gmail.com>
2019-12-28 09:18:31 +0700

    first push

---
+++ Triple_x/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  Triple_x
+//
+//  Created by sovannmonyreak hing on 12/27/19.
+//  Copyright © 2019 sovannmonyreak hing. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char *argv[])
+{
+    // insert code here...
+    cout << "Hello, World!\n";
+    cout << "You are a secret agent breaking into a scure server room!\n";
+    cout << "ENTER THE CORRECT CODE TO CONTINUE!\n";
+
+    int a = 4;
+    int b = 4;
+    int c = 4;
+
+    int sum = a * b *c ;
+    cout << "This is " << sum ;
+    return 0;
+}
